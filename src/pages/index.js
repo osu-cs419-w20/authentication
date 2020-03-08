da1f97ef98c14d68b0f62d4dc3a6a620cdd5f1f3
Rob Hess <robwhess@gmail.com>
2020-03-07 18:15:54 -0800

    Add initial code.

---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import fetch from 'isomorphic-unfetch';
+
+function Index() {
+  const [ user, setUser ] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch('/api/user');
+      const body = await res.json();
+      setUser(body);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Welcome!</h1>
+      {user.name && <p>Name: {user.name}</p>}
+      {user.email && <p>Email: {user.email}</p>}
+    </div>
+  );
+}
+
+export default Index;
